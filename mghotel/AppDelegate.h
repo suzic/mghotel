7dfe1a22c28a80e5b8cf4b2ce79154fbea499150
Suzic <suzicletter@msn.com>
2016-01-06 13:45:21 +0800

    Initial Commit

---
+++ mghotel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mghotel
+//
+//  Created by 苏智 on 16/1/6.
+//  Copyright © 2016年 Suzic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mghotel/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  mghotel
+//
+//  Created by 苏智 on 16/1/6.
+//  Copyright © 2016年 Suzic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
